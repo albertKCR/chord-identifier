75e78bff89bd03357ba47e30d69e34011306a5ca
albertKCR <kirchner_beto@hotmail.com>
2023-02-01 14:05:04 -0300

    major and minor chord

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+def major_chord():
+    for i in strings_to_check:
+        if (fret_notes[i]) == tonic or (fret_notes[i]) == third or (fret_notes[i]) == fifth:
+            continue
+        else:
+            print('Not a major chord')
+            return 0
+    return tonic
+    
+def minor_chord():
+    for i in strings_to_check:
+        if (fret_notes[i]) == tonic or (fret_notes[i]) == minor_third or (fret_notes[i]) == fifth:
+            continue
+        else:
+            print('Not a minor chord')
+            return 0
+    return tonic + 'm'
+
 notes = ('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B') #12
 
 tuning =[]
@@ -37,12 +55,12 @@ fret_notes_index = []
 # string_fret.append(input())
 # string_fret.append(input())
 
+string_fret.append(5)
+string_fret.append(4)
+string_fret.append(2)
+string_fret.append(2)
+string_fret.append(2)
 string_fret.append(-1)
-string_fret.append(5)
-string_fret.append(7)
-string_fret.append(7)
-string_fret.append(7)
-string_fret.append(5)
 
 
 for i in range(6):
@@ -56,26 +74,54 @@ print('Fret notes: ', fret_notes)
 print('Fret notes index in the notes vector: ', fret_notes_index)
 
 #verifies the tonic
-
+#in the worst case the tonic will be the fourth string, because need at least 3 notes to form the chord
 if string_fret[0] != -1:
     tonic_index = fret_notes_index[0]
     tonic = fret_notes[0]
+    tonic_string = 0
 elif string_fret[1] != -1:
     tonic_index = fret_notes_index[1]
     tonic = fret_notes[1]
+    tonic_string = 1
 elif string_fret[2] != -1:
     tonic_index = fret_notes_index[2]
     tonic = fret_notes[2]
+    tonic_string = 2
 elif string_fret[3] != -1:
     tonic_index = fret_notes_index[3]
     tonic = fret_notes[3]
+    tonic_string = 3
 
 print('Tonic: ', tonic)
 print('Tonic index in the notes list: ', tonic_index)
+print('Tonic string: ', tonic_string)
 
 #verifies which note is the third (tonic + 2 tone) and the fifth (tonic + 3 1/2 tones)
-
-third = notes[tonic_index+4]
+third_index = tonic_index + 4
+while third_index > 11:
+    third_index = third_index - 12
+third = notes[third_index]
 print('Third: ', third)
-fifth = notes[tonic_index+7]
+fifth_index = tonic_index + 7
+while fifth_index > 11:
+    fifth_index = fifth_index - 12
+fifth = notes[fifth_index]
 print('Fifth: ', fifth)
+
+#verifies the minor third
+minor_third_index = tonic_index + 3
+while minor_third_index > 11:
+    minor_third_index = minor_third_index - 12
+minor_third = notes[minor_third_index]
+print('Minor third: ', minor_third)
+
+#verifies which strings are played in the chord
+strings_to_check = []
+for i in range(6):
+    if string_fret[i]!=-1:
+        strings_to_check.append(i)
+print(strings_to_check)
+
+#verifies if its a major chord
+print(major_chord())
+print(minor_chord())
